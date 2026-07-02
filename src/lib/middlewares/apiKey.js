2c264f03c51ecc722ec082984e1f9b79c5f57112
Rafael-1108 <rafael.s.mendes.1108@gmail.com>
2026-07-02 00:13:58 -0300

    deploy concluido

--- src/lib/middlewares/apiKey.js
+++ src/lib/middlewares/apiKey.js
@@ -2,7 +2,7 @@ export const apiKey = (req, res, next) => {
     const key = req.headers['x-api-key'];
 
     if (!key || key !== process.env.API_KEY) {
-        return res.status(401).json({ error: 'API Key inválida ou ausente.' });
+        return res.status(401).json({ error: 'API Key inválida ou ausente.'});
     }
 
     next();
